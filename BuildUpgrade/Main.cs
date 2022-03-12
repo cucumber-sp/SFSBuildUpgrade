6a4515c5861108e06f5ca107112d8c42b3b2e201
Cucumber Space <loraner123@gmail.com>
2022-03-12 10:51:50 +0300

    Добавьте файлы проекта.

---
+++ BuildUpgrade/Main.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using ModLoader;
+using SFS;
+using System;
+using UnityEngine;
+
+namespace BuildUpgrade
+{
+    public class Main : SFSMod
+    {
+        public static GameObject menu = new GameObject("Cucumber Space");
+        public Main() : base("BuildUpgrade", "BuildUpgrade", "Cucumber Space", "v1.1.x", "Beta 1.1")
+        {
+        }
+
+        public static GameObject menuObject;
+
+        public override void load()
+        {
+            Helper.OnBuildSceneLoaded += Helper_OnBuildSceneLoaded;
+            applyPatches();
+            menu.AddComponent<Menu>();
+            UnityEngine.Object.DontDestroyOnLoad(menu);
+            Main.menuObject = menu;
+        }
+
+
+        private void Helper_OnBuildSceneLoaded(object sender, EventArgs _)
+        {
+            menu.SetActive(true);
+
+            foreach (string key in Base.partsLoader.colorTextures.Keys)
+            {
+                Base.partsLoader.colorTextures[key].tags = new string[] { "tank", "cone", "fairing" };
+            }
+
+            foreach (string key in Base.partsLoader.shapeTextures.Keys)
+            {
+                Base.partsLoader.shapeTextures[key].tags = new string[] { "tank", "cone", "fairing" };
+            }
+        }
+
+        public override void unload()
+        {
+        }
+
+        private void applyPatches() // is not necesary do whit static function
+        {
+            Harmony harmony = new Harmony("com.CucumberSpace.BuildUpgrade");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ BuildUpgrade/Patcher.cs
@@ -0,0 +1,102 @@
+﻿using HarmonyLib;
+using SFS;
+using SFS.Builds;
+using SFS.Parts.Modules;
+using UnityEngine;
+
+namespace BuildUpgrade
+{
+    public class Classes
+    {
+        public static HoldGrid ThisHoldGrid;
+
+        public static BuildGrid ThisBuildGrid;
+    }
+
+    [HarmonyPatch(typeof(HoldGrid), "GetSnapPosition_Old")]
+    public class HoldGrid_GetSnapPosition_Old
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(Vector2 position, out Vector2 __state)
+        {
+            __state = position;
+            return true;
+        }
+        [HarmonyPostfix]
+        public static void Postfix(ref Vector2 __result, Vector2 __state)
+        {
+            __result = BuildMethods.MyGetSnapPosition(__state, Classes.ThisHoldGrid);
+        }
+    }
+
+    [HarmonyPatch(typeof(HoldGrid), "Start")]
+    public class HoldGrid_Start
+    {
+        [HarmonyPrefix]
+        public static void Prefix(ref HoldGrid __instance)
+        {
+            Classes.ThisHoldGrid = __instance;
+        }
+    }
+
+    [HarmonyPatch(typeof(BuildGrid), "Start")]
+    public class BuildGrid_Start
+    {
+        [HarmonyPrefix]
+        public static void Prefix(ref BuildGrid __instance)
+        {
+            Classes.ThisBuildGrid = __instance;
+        }
+    }
+
+    [HarmonyPatch(typeof(AdaptModule), "Adapt")]
+    public class AdaptModule_Adapt
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            return Menu.isAdaptationOn;
+        }
+    }
+
+
+    [HarmonyPatch(typeof(Selector), "ToggleSelected")]
+    public class Selector_ToggleSelected
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            BuildMethods.RefreshPartValues();
+        }
+    }
+
+    [HarmonyPatch(typeof(Selector), "DeselectAll")]
+    public class Selector_DeselectAll
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            BuildMethods.RefreshPartValues();
+        }
+    }
+
+    [HarmonyPatch(typeof(HoldGrid), "EndHold")]
+    public class HoldGrid_EndHold
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            BuildMethods.RefreshPartValues();
+        }
+    }
+
+    [HarmonyPatch(typeof(BuildMenus), "OnEmptyClick")]
+    public class BuildMenus_OnEmptyClick
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            return Menu.IsDeselectOnEmptyClick;
+        }
+    }
+}
